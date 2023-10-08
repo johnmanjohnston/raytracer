d77d13772a3431cc3b3163741e2a6225e1f28d19
johnmanjohnston <c.ethanjohn@gmail.com>
2023-10-08 17:05:50 +0400

    add frag and vertex shader

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 
+// Setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 90, 1, 0.1, 1000 );
 const width = 700;
@@ -9,13 +10,23 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( width, height );
 document.body.appendChild( renderer.domElement );
 
-
+// Material definitions
 const redMaterial = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
 const greenMaterial = new THREE.MeshLambertMaterial({ color: 0x00FF00 });
 const whiteMaterial = new THREE.MeshLambertMaterial({ color: 0xFFFFFF });
 const grayMaterial = new THREE.MeshLambertMaterial({ color: 0xCCCCCC });
 
-const box = new THREE.Mesh(new THREE.BoxGeometry(1.5, 1.5, 1.5), greenMaterial);
+const colorUniform = { value: new THREE.Color(1,1,1) } 
+const boxMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        time: { value: 0.0 }
+    },
+    vertexShader: document.getElementById("vertexShader").textContent,
+    fragmentShader: document.getElementById("fragmentShader").textContent
+});
+
+// define center box
+const box = new THREE.Mesh(new THREE.BoxGeometry(1.5, 1.5, 1.5), boxMaterial);
 box.position.set(0, 0, 0);
 box.rotation.set(10, 10, 10);
 scene.add(box);
@@ -26,7 +37,7 @@ const ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
 ambientLight.position.set(0, 0, 0);
 
-
+// Create Cornell Box walls with this mess
 const WALL_DEPTH = 20;
 const WALL_DISTANCE_FROM_CENTER = 6;
 
@@ -39,11 +50,11 @@ rWall.position.set(WALL_DISTANCE_FROM_CENTER, 0, -5)
 scene.add(rWall);
 
 const roof = new THREE.Mesh(new THREE.BoxGeometry(15, 1, WALL_DEPTH), grayMaterial);
-roof.position.set(0, 6, -WALL_DISTANCE_FROM_CENTER)
+roof.position.set(0, 5, -WALL_DISTANCE_FROM_CENTER)
 scene.add(roof);
 
 const floor = new THREE.Mesh(new THREE.BoxGeometry(15, 1, WALL_DEPTH), grayMaterial);
-floor.position.set(0, -6, -WALL_DISTANCE_FROM_CENTER)
+floor.position.set(0, -5, -WALL_DISTANCE_FROM_CENTER)
 scene.add(floor);
 
 const backWall = new THREE.Mesh(new THREE.BoxGeometry(15, WALL_DEPTH + 5, 1), whiteMaterial);
@@ -55,13 +66,23 @@ frontWall.position.set(0, -0, WALL_DISTANCE_FROM_CENTER)
 scene.add(frontWall);
 
 const lightSource = new THREE.Mesh(new THREE.BoxGeometry(2, 0.1, 1), whiteMaterial);
-lightSource.position.set(0, 5, -WALL_DISTANCE_FROM_CENTER / 2);
+lightSource.position.set(0, 4, -WALL_DISTANCE_FROM_CENTER >> 1);
 scene.add(lightSource);
 
-const raycaster = new THREE.Raycaster();
+// now the juicy juicy stuff starts
+const BOUNCE_LIMIT = 1;
+var raycaster = new THREE.Raycaster();
+raycaster.set(camera.position, camera.getWorldDirection());
 
 const tick = () => {
     requestAnimationFrame(tick);
+    
+    boxMaterial.uniforms.time.value += 0.01;
+    var intersects = (raycaster.intersectObjects(scene.children, false));
+
+    // since a fragment shader runs for every pixel, we'll probably use that to create the 
+    // ray tracing effect.
+    
     renderer.render(scene, camera);
 };
 
